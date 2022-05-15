e89cb3d4178dc91c2899c4f518633dd596c8dbb8
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-05-15 11:55:11 -0400

    commit tree cec448dbf3185916dfab57906ac57f76ab500e86

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+  def index
+    students = Student.all
+    render json: students
+  end
+
+  def grades
+    students = Student.order("grade desc")
+    render json: students
+  end
+
+  def highest_grade
+    highest_grade = Student.order("grade desc").first
+    render json: highest_grade
+  end
+end
